bde5cf756ddb2b243c79a4645210f76ecf9c18ab
Eric Solari <beachandbytes@gmail.com>
2014-10-23 14:08:37 -1000

    Initial Commit

---
+++ FamilyProgramsHawaii/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FamilyProgramsHawaii.Startup))]
+namespace FamilyProgramsHawaii
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
